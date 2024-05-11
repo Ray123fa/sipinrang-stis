c10bc9c1c4d4213401e9fef910140e584f008496
Rayhan F <rayhanfrdh123@gmail.com>
2024-05-12 01:57:12 +0700

    update: - add handling exist username - add validation email domain - add getting id by username - add handling update profile information

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -9,6 +9,21 @@ class UserModel
 		$this->db = new Database;
 	}
 
+	public function isExistUsername($username)
+	{
+		$this->db->query('SELECT username FROM ' . $this->table . ' WHERE username = :username');
+		$this->db->bind(':username', $username);
+		$row = $this->db->single();
+
+		return ($row) ? true : false;
+	}
+
+	public function isEmailSTIS($email)
+	{
+		$domain = explode('@', $email)[1];
+		return ($domain == 'stis.ac.id') ? true : false;
+	}
+
 	public function getAllByUsername($username)
 	{
 		$this->db->query('SELECT * FROM ' . $this->table . ' WHERE username = :username');
@@ -16,6 +31,15 @@ class UserModel
 		$row = $this->db->single();
 
 		return $row;
+	}
+
+	public function getIdByUsername($username)
+	{
+		$this->db->query('SELECT id FROM ' . $this->table . ' WHERE username = :username');
+		$this->db->bind(':username', $username);
+		$row = $this->db->single();
+
+		return $row['id'];
 	}
 
 	public function getUnitByUsername($username)
@@ -36,8 +60,102 @@ class UserModel
 		return $row['level'];
 	}
 
-	public function editProfile($data, $username)
+	public function getProfileImgPath($username)
 	{
-		
+		$this->db->query('SELECT profile_img_path FROM ' . $this->table . ' WHERE username = :username');
+		$this->db->bind(':username', $username);
+		$row = $this->db->single();
+
+		return $row['profile_img_path'];
+	}
+
+	public function editProfile($data)
+	{
+		unset($_SESSION['user']);
+
+		if (!empty($_FILES['profile_img']['name'])) {
+			$fileNameOld = basename($_FILES['profile_img']['name']);
+			$fileType = strtolower(pathinfo($fileNameOld, PATHINFO_EXTENSION));
+
+			$fileNameNew = $data['username'] . '.' . $fileType;
+			$targetFile = 'upload/' . $fileNameNew;
+			$sourceFile = $_FILES['profile_img']['tmp_name'];
+			$allowedType = ['jpg', 'jpeg', 'png'];
+
+			// Cek ekstensi file
+			if (!in_array($fileType, $allowedType)) {
+				Flasher::setFlash('Ekstensi file tidak didukung', 'warning');
+				header('Location: ' . BASE_URL . 'user/profile');
+				exit;
+			}
+
+			// Cek ukuran file
+			if ($_FILES['profile_img']['size'] > 2 * 1024 * 1024) {
+				Flasher::setFlash('Ukuran file terlalu besar! Maksimal 2MB.', 'warning');
+				header('Location: ' . BASE_URL . 'user/profile');
+				exit;
+			}
+
+			// Hapus file lama
+			foreach (glob('upload/' . $data['username'] . '.(jpg|jpeg|png)') as $file) {
+				unlink($file);
+			}
+
+			// Pindahkan file ke folder upload
+			if (move_uploaded_file($sourceFile, $targetFile)) {
+				$this->db->query('UPDATE ' . $this->table . ' SET username = :username, email = :email, profile_img_path = :profile_img_path WHERE id = :id');
+
+				if ($this->isExistUsername($data['username'])) {
+					Flasher::setFlash('Username sudah terdaftar pada sistem!', 'warning');
+					header('Location: ' . BASE_URL . 'user/profile');
+					exit;
+				}
+
+				if (!$this->isEmailSTIS($data['email'])) {
+					Flasher::setFlash('Email harus berdomain STIS!', 'warning');
+					header('Location: ' . BASE_URL . 'user/profile');
+					exit;
+				}
+
+				$this->db->bind(':username', $data['username']);
+				$this->db->bind(':email', $data['email']);
+				$this->db->bind(':profile_img_path', $targetFile);
+				$this->db->bind(':id', $data['id']);
+				$this->db->execute();
+
+				$_SESSION['user'] = $data['username'];
+				Flasher::setFlash('Profil berhasil diubah', 'success');
+				header('Location: ' . BASE_URL . 'user/profile');
+				exit;
+			} else {
+				Flasher::setFlash('Profil gagal diubah', 'warning');
+				header('Location: ' . BASE_URL . 'user/profile');
+				exit;
+			}
+		} else {
+			$this->db->query('UPDATE ' . $this->table . ' SET username = :username, email = :email WHERE id = :id');
+
+			if ($this->isExistUsername($data['username'])) {
+				Flasher::setFlash('Username sudah terdaftar pada sistem!', 'warning');
+				header('Location: ' . BASE_URL . 'user/profile');
+				exit;
+			}
+
+			if (!$this->isEmailSTIS($data['email'])) {
+				Flasher::setFlash('Email harus berdomain STIS!', 'warning');
+				header('Location: ' . BASE_URL . 'user/profile');
+				exit;
+			}
+
+			$this->db->bind(':username', $data['username']);
+			$this->db->bind(':email', $data['email']);
+			$this->db->bind(':id', $data['id']);
+			$this->db->execute();
+
+			$_SESSION['user'] = $data['username'];
+			Flasher::setFlash('Profil berhasil diubah', 'success');
+			header('Location: ' . BASE_URL . 'user/profile');
+			exit;
+		}
 	}
 }
