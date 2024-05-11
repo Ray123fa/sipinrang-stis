1099113613a28d238e9fac0b80c4ca7a195a7c8f
Rayhan F <rayhanfrdh123@gmail.com>
2024-05-12 01:50:22 +0700

    update: add handling file size

--- public/js/Dashboard/Profile.js
+++ public/js/Dashboard/Profile.js
@@ -1,6 +1,14 @@
 const profile_img = document.getElementById("profile_img");
 profile_img.addEventListener("change", function () {
   const file = this.files[0];
+
+  // Jika ukuran file lebih dari 2MB
+  if (file.size > 2*1024*1024) {
+    alert("Ukuran file terlalu besar! Maksimal 2MB.");
+    this.value = "";
+    return;
+  }
+
   if (file) {
     const reader = new FileReader();
     reader.onload = function () {
