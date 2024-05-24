0271843b098c83888e9207599774d278fa37a9df
Rayhan F <rayhanfrdh123@gmail.com>
2024-05-25 01:32:02 +0700

    feat: Add generateId() method to PeminjamanModel

--- app/models/PeminjamanModel.php
+++ app/models/PeminjamanModel.php
@@ -9,6 +9,21 @@ class PeminjamanModel
 		$this->db = new Database;
 	}
 
+	// Generate ID Peminjaman
+	public function generateId($l = 5)
+	{
+		$str = "";
+		$characters = array_merge(range('A', 'Z'), range('a', 'z'), range('0', '9'));
+		$max = count($characters) - 1;
+
+		for ($i = 0; $i < $l; $i++) {
+			$rand = mt_rand(0, $max);
+			$str .= $characters[$rand];
+		}
+
+		return $str;
+	}
+
 	// Semua Peminjaman
 	public function getAll()
 	{
@@ -102,12 +117,43 @@ class PeminjamanModel
 	}
 
 	// Detail Peminjaman
-	public function getPeminjamanByIDPinjam($id_pinjam)
+	public function getPeminjamanById($id)
+	{
+		$this->db->query('SELECT * FROM ' . $this->table . ' WHERE id = :id');
+		$this->db->bind(':id', $id);
+		$row = $this->db->single();
+
+		return $row;
+	}
+
+	public function getPeminjamanByIdPinjam($id_pinjam)
 	{
 		$this->db->query('SELECT * FROM ' . $this->table . ' WHERE id_pinjam = :id_pinjam');
 		$this->db->bind(':id_pinjam', $id_pinjam);
 		$row = $this->db->single();
 
 		return $row;
+	}
+
+	// Tambah Peminjaman
+	public function tambahPeminjaman($data, $unit)
+	{
+		$query = "INSERT INTO " . $this->table . " VALUES (:id, :id_pinjam, :kegiatan, :dresscode, :unit, :dibuat_tanggal, :diperlukan_tanggal, :ruang, :sesi, :status, :keterangan, :last_update)";
+		$this->db->query($query);
+		$this->db->bind(':id', null);
+		$this->db->bind(':id_pinjam', $this->generateId());
+		$this->db->bind(':kegiatan', $data['kegiatan']);
+		$this->db->bind(':dresscode', $data['dresscode']);
+		$this->db->bind(':unit', $unit);
+		$this->db->bind(':dibuat_tanggal', date('Y-m-d'));
+		$this->db->bind(':diperlukan_tanggal', $data['diperlukan_tanggal']);
+		$this->db->bind(':ruang', $data['ruang']);
+		$this->db->bind(':sesi', $data['sesi']);
+		$this->db->bind(':status', 1);
+		$this->db->bind(':keterangan', '');
+		$this->db->bind(':last_update', date('Y-m-d H:i:s'));
+		$this->db->execute();
+
+		return $this->db->rowCount();
 	}
 }
