bb0ffb6e629f0c989d9b55b05e6e362ec75bda3d
Rayhan F <rayhanfrdh123@gmail.com>
2024-05-25 01:16:25 +0700

    update: change db_name

--- app/config/Config.php
+++ app/config/Config.php
@@ -1,9 +1,8 @@
 <?php
 define('BASE_URL', 'https://sipinrang.dev/');
-
 define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', 'root');
-define('DB_NAME', 'sipinrang_stis');
+define('DB_NAME', 'newsipinrang');
 define('DB_CHARSET', 'utf8');
