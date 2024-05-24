c7230d56cf394f46702a9ef6c3b19e1596c63bcd
Rayhan F <rayhanfrdh123@gmail.com>
2024-05-25 01:26:15 +0700

    chore: Add optional 'place' parameter to setFlash() method

--- app/core/Flasher.php
+++ app/core/Flasher.php
@@ -1,24 +1,27 @@
 <?php
 class Flasher
 {
-	public static function setFlash($message, $type)
+	public static function setFlash($message, $type, $place = null)
 	{
 		$_SESSION['flash'] = [
 			'message' => $message,
-			'type' => $type
+			'type' => $type,
+			'place' => $place
 		];
 	}
 
-	public static function flash()
+	public static function flash($for = null)
 	{
 		if (isset($_SESSION['flash'])) {
-			echo '<div class="d-flex justify-content-between align-items-center alert alert-' . $_SESSION['flash']['type'] . ' mb-2">'
-				. $_SESSION['flash']['message'] . '
+			if ($_SESSION['flash']['place'] == $for) {
+				echo '<div class="d-flex justify-content-between align-items-center alert alert-' . $_SESSION['flash']['type'] . ' mb-2">'
+					. $_SESSION['flash']['message'] . '
 				<span class="cursor-pointer p-1" onclick="this.parentElement.style.display = \'none\'">
 					<i class="fas fa-xmark"></i>
 				</span>
 				</div>';
-			unset($_SESSION['flash']);
+				unset($_SESSION['flash']);
+			}
 		}
 	}
 }
