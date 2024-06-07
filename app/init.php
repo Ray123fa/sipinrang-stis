b6aa5936ba60cf03895b5b74261f39a61cb6ad06
Rayhan F <rayhanfrdh123@gmail.com>
2024-06-07 22:14:33 +0700

    chore: Remove session related code from init.php

--- app/init.php
+++ app/init.php
@@ -1,6 +1,4 @@
 <?php
-ini_set('session.gc_maxlifetime', 60 * 60);
-session_set_cookie_params(60 * 60);
 ini_set('display_errors', 1); // 0 in production
 date_default_timezone_set('Asia/Jakarta');
 
