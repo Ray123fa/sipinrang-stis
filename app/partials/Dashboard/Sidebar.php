d116f47027b21a4d191c9e2a814bddfcfeb7f42c
Rayhan F <rayhanfrdh123@gmail.com>
2024-06-07 22:13:53 +0700

    Refactor sidebar links to use full width for better user experience

--- app/partials/Dashboard/Sidebar.php
+++ app/partials/Dashboard/Sidebar.php
@@ -5,19 +5,37 @@
 	</div>
 	<ul class="sidebar-list list-style-none cursor-pointer">
 		<li class="d-flex align-items-center p-5px border-radius-1 mb-2 <?= ($data['title'] == 'Dashboard') ? 'active' : ''; ?>">
-			<i class="fas fa-home mr-2"></i>
-			<a href="user">Beranda</a>
+			<a href="user">
+				<i class="fas fa-home mr-2"></i>
+				<a href="user" class="w-full">Beranda</a>
+			</a>
 		</li>
+		<li class="d-flex align-items-center p-5px border-radius-1 mb-2 <?= ($data['title'] == 'Tambah Peminjaman') ? 'active' : ''; ?>">
+			<a href="user/tambah-peminjaman">
+				<i class="fas fa-plus mr-2"></i>
+				<a href="user/tambah-peminjaman" class="w-full">Tambah Peminjaman</a>
+			</a>
+		</li>
+		<?php if ($data['level'] != 2) : ?>
+			<li class="d-flex align-items-center p-5px border-radius-1 mb-2 <?= ($data['title'] == 'Daftar Pengguna') ? 'active' : ''; ?>">
+				<a href="user/daftar-pengguna">
+					<i class="fas fa-list mr-2"></i>
+					<a href="user/daftar-pengguna" class="w-full">Daftar Pengguna</a>
+				</a>
+			</li>
+		<?php endif; ?>
 		<li class="d-flex align-items-center p-5px border-radius-1 mb-2 <?= ($data['title'] == 'Semua Peminjaman') ? 'active' : ''; ?>">
-			<i class="fas fa-list mr-2"></i>
-			<a href="user/all_peminjaman">Semua Peminjaman</a>
+			<a href="user/all-peminjaman">
+				<i class="fas fa-list mr-2"></i>
+				<a href="user/all-peminjaman" class="w-full">Semua Peminjaman</a>
+			</a>
 		</li>
 		<li class="d-flex align-items-center p-5px border-radius-1 mb-2 <?= ($data['title'] == 'Peminjaman Saya') ? 'active' : ''; ?>">
-			<i class="fas fa-list mr-2"></i>
-			<a href="user/my_peminjaman">Peminjaman Saya</a>
+			<a href="user/my-peminjaman">
+				<i class="fas fa-list mr-2"></i>
+				<a href="user/my-peminjaman" class="w-full">Peminjaman Saya</a>
+			</a>
 		</li>
-		<li class="p-5px border-radius-1 mb-2">woi</li>
-		<li class="p-5px border-radius-1 mb-2">woi</li>
 	</ul>
 </aside>
 <!-- Sidebar End -->
