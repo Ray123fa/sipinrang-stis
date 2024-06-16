88c55305cbce97fc46afa4456865573c0ba76e59
Rayhan F <rayhanfrdh123@gmail.com>
2024-06-17 01:20:08 +0700

    chore: Update meta tags in header files

--- app/partials/Welcome/Header.php
+++ app/partials/Welcome/Header.php
@@ -3,7 +3,11 @@
 
 <head>
 	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="Muhammad Rayhan Faridh">
+	<meta name="keywords" content="sipinrang, sistem informasi peminjaman ruang, peminjaman ruang, stis, stis.ac.id, stis.ac.id/sipinrang">
+	<meta name="description" content="Sistem Informasi Peminjaman Ruang STIS">
 	<base href="<?= BASE_URL ?>">
 	<title>Sipinrang STIS <?php echo (isset($data['title'])) ? '|| ' . $data['title'] : ''; ?></title>
 
